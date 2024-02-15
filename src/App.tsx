f225640eb213ad00b0e6baf26825574cf233870f
johnproumas <iproumas@gmail.com>
2024-02-15 10:15:41 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount((count) => count + 1);
+  }
+  function decrement() {
+    setCount((count) => count - 1);
+  }
+
+  return (
+    <>
+      <div>
+        <a
+          href="https://vitejs.dev"
+          target="_blank"
+        >
+          <img
+            src={viteLogo}
+            className="logo"
+            alt="Vite logo"
+          />
+        </a>
+        <a
+          href="https://react.dev"
+          target="_blank"
+        >
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+          />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card flex">
+        <button onClick={decrement}>-</button>
+        <p>Count is: {count}</p>
+        <button onClick={increment}>+</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
